68d556ec49241509a1fe6f3d753ccf4859e77835
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2019-01-26 19:46:29 +0100

    Add Respomse Holder

--- js/script.js
+++ js/script.js
@@ -7,11 +7,16 @@ var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
+var waitTillRecieve = false;
+
 document.addEventListener('DOMContentLoaded', function() {
 	    getQuote();
-	    document.querySelector('.trigger').addEventListener('click', function() {
-	        getQuote();
-	    });
+        if (waitTillRecieve == false) {
+            waitTillRecieve = true;
+    	    document.querySelector('.trigger').addEventListener('click', function() {
+    	        getQuote();
+    	    });
+        }
 	});
 
 function getQuote() {
@@ -44,7 +49,7 @@ function createTweet(input) {
 	    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
 	    document.querySelector('.tweet').setAttribute('href', tweet);
 	}
-
+    waitTillRecieve = false;
 }
 
 })();
